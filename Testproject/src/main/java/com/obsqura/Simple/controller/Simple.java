50f29bb426c21b9057b4f16be1949bac1c15f04f
NETCOM <vinayarajeevan750@gmail.com>
2026-06-18 19:35:45 +0530

    spring mvc

---
+++ Testproject/src/main/java/com/obsqura/Simple/controller/Simple.java
@@ -0,0 +1,12 @@
+package com.obsqura.Simple.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Simple {
+	@GetMapping("/hello")
+    public String sayHello() {
+        return "hello"; // goes to hello.jsp
+}
+}
---
+++ Testproject/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<a href="hello">View Greetings</a>
+<h2>Hello World!</h2>
+</body>
+</html>
